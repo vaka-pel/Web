ed25b115c3943d2acfb8985f1dc86aba6e7ac146
Pelivan <Pelivan@DESKTOP-29KL5UV>
2026-07-20 21:25:24 +0300

    TODO: place factorial result

--- JavaScript/forms_and_events.js
+++ JavaScript/forms_and_events.js
@@ -3,9 +3,11 @@ function Factorial() {
     let numberElement = document.getElementById("number");
     let number = numberElement.value;
     let resultElement = document.getElementById("factorial-result");
-    let f = 1;
-    for (let i = 1; i <= number; i++) {
+    let f = BigInt(1);
+    for (let i = 1n; i <= number; i++)
+    {
         f *= i;
     }
-    resultElement.innerHTML = f;
+   /* resultElement.value = `${number}! = ${f}`;*/
+    resultElement.innerHTML = `${number}! = ${f}`;
 }

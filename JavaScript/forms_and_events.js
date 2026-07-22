2944c88cdebd20bcedcf14acc3707643e7fb74f8
Pelivan <Pelivan@DESKTOP-29KL5UV>
2026-07-22 20:50:46 +0300

    Colors: DONE

--- JavaScript/forms_and_events.js
+++ JavaScript/forms_and_events.js
@@ -11,3 +11,28 @@ function Factorial() {
    /* resultElement.value = `${number}! = ${f}`;*/
     resultElement.innerHTML = `${number}! = ${f}`;
 }
+
+function setImage()
+{
+    let filename = document.getElementById("image-file");
+    let reader = new FileReader();
+    reader.onload = function (e)
+    {
+        document.getElementById("image").src = e.target.result;
+    }
+    reader.readAsDataURL(filename.files[0]);
+}
+function setBackgroundColor()
+
+{
+    document.body.style.backgroundColor = document.getElementById("background-color").value;
+    //let color = document.getElementById("bakcground-color").value;
+    //document.body.style.backgroundColor = color;
+    //alert("setBackgroundColor");
+}
+function setForegroundColor()
+
+{
+    document.body.style.color = document.getElementById("foreground-color").value;
+   
+}
